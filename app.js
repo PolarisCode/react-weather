808794096ba31c75131ca3deff7185e327f52073
Imran Aliyev <ialiyev@MacBook-Pro-Eiu.local>
2016-02-24 08:38:35 +0400

    Bootstrap module added. Middleware assignments were added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get("/test", function(req, res){
-  res.send("It Works!");
-});
+app.use('/', express.static(__dirname+'/public'));
+app.use('/api', express.static(__dirname + '/api'));
+app.use('/js/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/css/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 
 app.listen(3000, function(){
-  console.log("Application API started on localhost:3000");
+  console.log("App started on localhost:3000");
 });
